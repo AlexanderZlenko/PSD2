e3bbbc07799f9a670a9efd72e3a0dc4684646e93
AlexanderZlenko <37269821+AlexanderZlenko@users.noreply.github.com>
2019-04-03 18:06:55 +0300

    Add files via upload

---
+++ js/script.min.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider__item");
+    let dots = document.getElementsByClassName("slider-dots__item");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// бургер-меню
+
+document.getElementById('trigger').onclick = function() {
+    open()
+};
+
+function open() {
+    document.getElementById('main-nav').classList.toggle('show');
+}
